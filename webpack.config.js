e126cb229a3fdaaad80fbec1139af1d85edfcd30
Mukul K Singh <mukul.intel@gmail.com>
2017-02-10 09:30:14 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" }
+    ]
+  }
+};
